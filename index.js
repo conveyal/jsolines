bf61efeb490e12914675bda0b9ca7cfa616a7aa1
Trevor Gerhardt <trevorgerhardt@gmail.com>
2018-11-29 13:00:36 +0800

    feat(coordinates): Increase maximum allowable coordinates default to 20000 and allow passing it as a

--- index.js
+++ index.js
@@ -8,48 +8,52 @@
 
 import {point} from '@turf/helpers'
 import inside from '@turf/boolean-point-in-polygon'
-import dbg from 'debug'
 
 type Coordinate = [number, number]
 
-const debug = dbg('jsolines')
+const MAX_COORDS = 20000
+
+// Previously we used `debug`
+const logError = e => console.error(e)
 
 /**
  * Create a JSON isoline. Surface is a (possibly typed) array, width and height
  * are its width and height, and cutoff is the cutoff. It is possible to disable
- * linear interpolation for debug purposes by passing interpolation: false
+ * linear interpolation for testing purposes by passing interpolation: false.
  */
 export default function jsolines ({
-  surface,
-  width,
-  height,
   cutoff,
+  height,
+  maxCoordinates = MAX_COORDS,
   project,
-  interpolation = true
+  interpolation = true,
+  surface,
+  width
 }: {
   cutoff: number,
   height: number,
   interpolation: boolean,
+  maxCoordinates: number,
   project: (Coordinate) => Coordinate,
   surface: Uint8Array,
   width: number
 }) {
-  // first, create the contour grid
+  // First, create the contour grid.
   const contour = getContour({surface, width, height, cutoff})
   const cWidth = width - 1
 
-  // javascript does not have boolean arrays. lame.
+  // JavaScript does not have boolean arrays. lame.
   const found = new Uint8Array((width - 1) * (height - 1))
 
   // DEBUG, comment out to save memory
   const indices = []
 
-  // we'll sort out what shell goes with what hole in a bit
+  // We'll sort out what shell goes with what hole in a bit.
   const shells = []
   const holes = []
 
-  // find a cell that has a line in it, then follow that line, keeping filled area to your
-  // left. This lets us use winding direction to determine holes.
+  // Find a cell that has a line in it, then follow that line, keeping filled
+  // area to your left. This lets us use winding direction to determine holes.
   for (let origy = 0; origy < height - 1; origy++) {
     for (let origx = 0; origx < width - 1; origx++) {
       if (found[origy * cWidth + origx] === 1) {
@@ -57,10 +61,10 @@ export default function jsolines ({
       }
 
       let idx = contour[origy * cWidth + origx]
-      // continue if there is no line here or if it's a saddle, as we don't know which way the saddle goes
+      // Continue if there is no line here or if it's a saddle, as we don't know which way the saddle goes.
       if (idx === 0 || idx === 5 || idx === 10 || idx === 15) continue
 
-      // huzzah! we have found a line, now follow it, keeping the filled area to our left,
+      // Huzzah! We have found a line, now follow it, keeping the filled area to our left,
       // which allows us to use the winding direction to determine what should be a shell and
       // what should be a hole
       let x = origx
@@ -71,17 +75,17 @@ export default function jsolines ({
       let startx = -1
       let starty = -1
 
-      // track winding direction
+      // Track winding direction
       let direction = 0
 
       const coords = []
 
       while (true) {
-        // make sure we're not traveling in circles
+        // Make sure we're not traveling in circles.
         // NB using index from _previous_ cell, we have not yet set an index for this cell
         if (found[y * cWidth + x] === 1) {
-          debug(`Ring crosses other ring (or possibly self) at ${x}, ${y} coming from case ${idx}`)
-          debug(`Last few indices: ${indices.slice(Math.max(0, indices.length - 10)).join(',')}`)
+          logError(`Ring crosses other ring (or possibly self) at ${x}, ${y} coming from case ${idx}`)
+          logError(`Last few indices: ${indices.slice(Math.max(0, indices.length - 10)).join(',')}`)
           break
         }
 
@@ -93,107 +97,41 @@ export default function jsolines ({
 
         indices.push(idx)
 
-        // only mark as found if it's not a saddle because we expect to reach saddles twice.
+        // Mark as found if it's not a saddle because we expect to reach saddles twice.
         if (idx !== 5 && idx !== 10) {
           found[y * cWidth + x] = 1
         }
 
         if (idx === 0 || idx === 15) {
-          debug('Ran off outside of ring')
+          logError('Ran off outside of ring')
           break
         }
 
-        // follow the loop
-        // we keep track of which contour cell we're in, and we always keep the filled area to our left.
-        // thus we always indicate only which direction we exit the cell.
-        switch (idx) {
-          case 1:
-            x--
-            break
-          // NB: +y is down
-          case 2:
-            y++
-            break
-          case 3:
-            x--
-            break
-          case 4:
-            x++
-            break
-          case 5:
-            // assume that saddle has // orientation (as opposed to \\). It doesn't
-            // really matter if we're wrong, we'll just have two disjoint pieces where we should have
-            // one, or vice versa
-            if (prevy > y) {
-              // came from bottom
-              x++
-            } else if (prevy < y) {
-              // came from top
-              x--
-            } else {
-              debug('Entered case 5 saddle point from wrong direction!')
-            }
-            break
-          case 6:
-            y++
-            break
-          case 7:
-            x--
-            break
-          case 8:
-            y--
-            break
-          case 9:
-            y--
-            break
-          case 10: // hex a
-            if (prevx < x) {
-              // came from left
-              y++
-            } else if (prevx > x) {
-              // came from right
-              y--
-            } else {
-              debug('Entered case 10 saddle point from wrong direction.')
-            }
-            break
-          case 11: // b
-            y--
-            break
-          case 12: // c
-            x++
-            break
-          case 13: // d
-            x++
-            break
-          case 14: // e
-            y++
-            break
-        }
+        // Follow the loop
+        [x, y] = followLoop({idx, prevx, prevy, x, y})
 
-        // keep track of winding direction
+        // Keep track of winding direction
         direction += (x - startx) * (y + starty)
 
-        const coord = interpolate({
-          coord: [x, y],
-          cutoff,
-          interpolation,
-          startx,
-          starty,
-          surface,
-          width,
-          height
-        })
-        if (!coord) break
+        // Shift exact coordinates
+        const coord = interpolation
+          ? interpolate({x, y, cutoff, startx, starty, surface, width, height})
+          : noInterpolate({x, y, startx, starty})
+
+        if (!coord) {
+          logError(`Unexpected coordinate shift from ${startx}, ${starty} to ${x}, ${y}, discarding ring`)
+          break
+        }
 
         coords.push(project(coord))
 
-        if (coords.length > 10000) {
-          debug('More than 10000 coordinates found in ring, skipping this ring')
+        // TODO Remove completely? May be unnecessary.
+        if (coords.length > maxCoordinates) {
+          logError(`Ring coordinates > ${maxCoordinates} found, skipping`)
           break
         }
 
-        // we're back at the start of the ring
+        // We're back at the start of the ring
         if (x === origx && y === origy) {
           coords.push(coords[0]) // close the ring
 
@@ -206,9 +144,9 @@ export default function jsolines ({
             }
           }
 
-          // check winding direction
-          // positive means counter clockwise, see http://stackoverflow.com/questions/1165647
-          // NB +y is down so the signs are reversed from what would normally be expected
+          // Check winding direction. Positive here means counter clockwise,
+          // see http://stackoverflow.com/questions/1165647
+          // +y is down so the signs are reversed from what would be expected
           if (direction > 0) shells.push(geom)
           else holes.push(geom)
           break
@@ -217,19 +155,18 @@ export default function jsolines ({
     }
   }
 
-  // shell game time.
-  // sort out shells and holes
+  // Shell game time. Sort out shells and holes.
   holes.forEach((hole) => {
-    // NB this is checking whether the first coordinate of the hole is inside the shell.
-    // This is sufficient as shells don't overlap, and holes are guaranteed to be completely
-    // contained by a single shell.
+    // NB this is checking whether the first coordinate of the hole is inside
+    // the shell. This is sufficient as shells don't overlap, and holes are
+    // guaranteed to be completely contained by a single shell.
     const holePoint = point(hole.geometry.coordinates[0][0])
     const containingShell = shells.find((shell) => inside(holePoint, shell))
 
     if (containingShell) {
       containingShell.geometry.coordinates.push(hole.geometry.coordinates[0])
     } else {
-      debug('Did not find fitting shell for hole')
+      logError('Did not find fitting shell for hole')
     }
   })
 
@@ -242,87 +179,142 @@ export default function jsolines ({
   }
 }
 
+/**
+ * Follow the loop
+ * We keep track of which contour cell we're in, and we always keep the filled
+ * area to our left. Thus we always indicate only which direction we exit the
+ * cell.
+ */
+function followLoop ({idx, prevx, prevy, x, y}) {
+  switch (idx) {
+    case 1: return [x - 1, y]
+    // NB: +y is down
+    case 2: return [x, y + 1]
+    case 3: return [x - 1, y]
+    case 4: return [x + 1, y]
+    case 5:
+      // Assume that saddle has // orientation (as opposed to \\). It doesn't
+      // really matter if we're wrong, we'll just have two disjoint pieces
+      // where we should have one, or vice versa.
+
+      // From bottom
+      if (prevy > y) return [x + 1, y]
+
+      // From top
+      if (prevy < y) return [x - 1, y]
+
+      logError('Entered case 5 saddle point from wrong direction!')
+      return [x, y]
+    case 6: return [x, y + 1]
+    case 7: return [x - 1, y]
+    case 8: return [x, y - 1]
+    case 9: return [x, y - 1]
+    case 10: // hex a
+      // From left
+      if (prevx < x) return [x, y + 1]
+
+      // From right
+      if (prevx > x) return [x, y - 1]
+
+      logError('Entered case 10 saddle point from wrong direction.')
+      return [x, y]
+    case 11: return [x, y - 1] // b
+    case 12: return [x + 1, y] // c
+    case 13: return [x + 1, y] // d
+    case 14: return [x, y + 1] // e
+  }
+}
+
+// Calculated fractions may not be numbers causing interpolation to fail.
+const ensureFractionIsNumber = (frac, direction) => {
+  if (isNaN(frac) || frac === Infinity) {
+    logError(`Segment fraction from ${direction} is ${frac}; if this is at the edge of the query this is expected.`)
+    return 0.5
+  }
+  return frac
+}
+
+/**
+ * Do linear interpolation.
+ */
 function interpolate ({
-  coord,
   cutoff,
-  interpolation,
+  height,
   startx,
   starty,
   surface,
   width,
-  height
+  x,
+  y
 }: {
-  coord: Coordinate,
   cutoff: number,
   height: number,
-  interpolation: boolean,
   startx: number,
   starty: number,
   surface: Uint8Array,
-  width: number
+  width: number,
+  x: number,
+  y: number
 }): (Coordinate | void) {
-  const [x, y] = coord
   const index = y * width + x
   let topLeft = surface[index]
   let topRight = surface[index + 1]
   let botLeft = surface[index + width]
   let botRight = surface[index + width + 1]
 
-  // The edges are always considered unreachable to avoid edge effects
-  // so set them to the cutoff
+  // The edges are always considered unreachable to avoid edge effects so set
+  // them to the cutoff.
   if (x === 0) topLeft = botLeft = cutoff
   if (y === 0) topLeft = topRight = cutoff
   if (y === height - 2) botRight = botLeft = cutoff
   if (x === width - 2) topRight = botRight = cutoff
 
-  // do linear interpolation
+  // From left
   if (startx < x) {
-    // came from left
-    let frac = interpolation ? (cutoff - topLeft) / (botLeft - topLeft) : 0.5
+    const frac = (cutoff - topLeft) / (botLeft - topLeft)
+    return [x, y + ensureFractionIsNumber(frac, 'left')]
+  }
 
-    if (isNaN(frac) || frac === Infinity) {
-      debug(`segment fraction from left is ${frac} at ${x}, ${y}; if this is at the edge of the query this is expected.`)
-      frac = 0.5
-    }
+  // From right
+  if (startx > x) {
+    const frac = (cutoff - topRight) / (botRight - topRight)
+    return [x + 1, y + ensureFractionIsNumber(frac, 'right')]
+  }
 
-    return [x, y + frac]
-  } else if (startx > x) {
-    // came from right
-    let frac = interpolation ? (cutoff - topRight) / (botRight - topRight) : 0.5
+  // From bottom
+  if (starty > y) {
+    const frac = (cutoff - botLeft) / (botRight - botLeft)
+    return [x + ensureFractionIsNumber(frac, 'bottom'), y + 1]
+  }
 
-    if (isNaN(frac) || frac === Infinity) {
-      debug(`segment fraction from right is ${frac} at ${x}, ${y}; if this is at the edge of the query this is expected.`)
-      frac = 0.5
-    }
-
-    return [x + 1, y + frac]
-  } else if (starty > y) {
-    // came from bottom
-    let frac = interpolation ? (cutoff - botLeft) / (botRight - botLeft) : 0.5
-
-    if (isNaN(frac) || frac === Infinity) {
-      debug(`segment fraction from bottom is ${frac} at ${x}, ${y}; if this is at the edge of the query this is expected.`)
-      frac = 0.5
-    }
-
-    return [x + frac, y + 1]
-  } else if (starty < y) {
-    // came from top
-    let frac = interpolation ? (cutoff - topLeft) / (topRight - topLeft) : 0.5
-
-    if (isNaN(frac) || frac === Infinity) {
-      debug(`segment fraction from top is ${frac} at ${x}, ${y}; if this is at the edge of the query this is expected.`)
-      frac = 0.5
-    }
-
-    return [x + frac, y]
-  } else {
-    debug(`Unexpected coordinate shift from ${startx}, ${starty} to ${x}, ${y}, discarding ring`)
+  // From top
+  if (starty < y) {
+    const frac = (cutoff - topLeft) / (topRight - topLeft)
+    return [x + ensureFractionIsNumber(frac, 'top'), y]
   }
 }
 
 /**
- * Get a contouring grid. Exported for debug purposes, not generally used
+ * Used for testing.
+ */
+function noInterpolate ({startx, starty, x, y}: {
+  startx: number,
+  starty: number,
+  x: number,
+  y: number
+}): (Coordinate | void) {
+  // From left
+  if (startx < x) return [x, y + 0.5]
+  // From right
+  if (startx > x) return [x + 1, y + 0.5]
+  // From bottom
+  if (starty > y) return [x + 0.5, y + 1]
+  // From top
+  if (starty < y) return [x + 0.5, y]
+}
+
+/**
+ * Get a contouring grid. Exported for testing purposes, not generally used
  * outside jsolines testing
  */
 export function getContour ({
